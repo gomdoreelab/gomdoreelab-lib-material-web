1f17d3142fd6878947bb3e33f3b7768f1ccc39a8
laminar <laminar@kakao.com>
2025-02-01 13:50:29 +0900

    [FIX] ISSUE #13

--- src/lib/components/app-bar/Top.svelte
+++ src/lib/components/app-bar/Top.svelte
@@ -2,25 +2,9 @@
 	import 'mdui/mdui.css';
 	import 'mdui/components/top-app-bar.js';
 
-	let {
-		// Properties
-		order = 0,
-		// Slots
-		children,
-		...props
-	} = $props();
-
-	let drawer = document.querySelector('mdui-navigation-drawer');
-	let rail = document.querySelector('mdui-navigation-rail');
-	let width = drawer ? drawer.style.width : rail ? rail.style.width : '80';
+	let { children, ...props } = $props();
 </script>
 
-{#if order === 0}
-	<svelte:element this={'mdui-top-app-bar'} {...props} style="width: 100%;">
-		{@render children?.()}
-	</svelte:element>
-{:else}
-	<svelte:element this={'mdui-top-app-bar'} {...props} style="width: calc(100% - {width}px);">
-		{@render children?.()}
-	</svelte:element>
-{/if}
+<svelte:element this={'mdui-top-app-bar'} {...props} style="width: 100%;">
+	{@render children?.()}
+</svelte:element>
