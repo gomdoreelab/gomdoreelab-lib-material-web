f71dd2c6f6d7846013ce382f3136d8ae365ad90d
laminar <laminar@kakao.com>
2025-02-01 23:32:09 +0900

    [FIX] ISSUE #18

--- src/lib/components/navigation/Rail.svelte
+++ src/lib/components/navigation/Rail.svelte
@@ -2,9 +2,11 @@
 	import 'mdui/mdui.css';
 	import 'mdui/components/navigation-rail.js';
 
-	let { children, ...props } = $props();
+	let { children, _top, _bottom, ...props } = $props();
 </script>
 
 <svelte:element this={'mdui-navigation-rail'} {...props} style="height: 100%;">
 	{@render children?.()}
+	{@render _top?.()}
+	{@render _bottom?.()}
 </svelte:element>
