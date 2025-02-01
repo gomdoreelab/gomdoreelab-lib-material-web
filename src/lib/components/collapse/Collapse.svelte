1bd55fcc38d84e6578f82e9cb31c407b78c7c50a
laminar <laminar@kakao.com>
2025-02-01 23:17:38 +0900

    [FIX] ISSUE #17

--- src/lib/components/collapse/Collapse.svelte
+++ src/lib/components/collapse/Collapse.svelte
@@ -2,10 +2,9 @@
 	import 'mdui/mdui.css';
 	import 'mdui/components/collapse.js';
 
-	let { children, _header, ...props } = $props();
+	let { children, ...props } = $props();
 </script>
 
 <svelte:element this={'mdui-collapse'} {...props}>
 	{@render children?.()}
-	{@render _header?.()}
 </svelte:element>
--- src/lib/components/collapse/Item.svelte
+++ src/lib/components/collapse/Item.svelte
@@ -2,9 +2,10 @@
 	import 'mdui/mdui.css';
 	import 'mdui/components/collapse-item.js';
 
-	let { children, ...props } = $props();
+	let { children, _header, ...props } = $props();
 </script>
 
 <svelte:element this={'mdui-collapse-item'} {...props}>
 	{@render children?.()}
+	{@render _header?.()}
 </svelte:element>
